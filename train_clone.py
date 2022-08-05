387377cdfe06fd2361875c167cbc24b2775e59a7
thor-muscle <dlwoguddd@naver.com>
2022-08-05 17:42:41 +0900

    train.py 보는중

--- train_clone.py
+++ train_clone.py
@@ -64,7 +64,8 @@ def patch_trg(trg, pad_idx):
     trg, gold = trg[:, :-1], trg[:, 1:].contiguous().view(-1)
     return trg, gold
 
-
+# training_data 누가 들어오길래, .src, .trg 등을 할 수 있는거야?
+# 어떻게 처리되길래, batch 나 epoch 언급없이 학습이 되는거지? 한 epoch에 대한건가?
 def train_epoch(model, training_data, optimizer, opt, device, smoothing):
     """Epoch operation in training phase"""
     
@@ -75,22 +76,78 @@ def train_epoch(model, training_data, optimizer, opt, device, smoothing):
     for batch in tqdm(training_data, mininteraval=2, desc=desc, leave=False):
         
         # prepare data
+        src_seq = patch_src(batch.src, opt.src_pad_idx).to(device)
+        trg_seq, gold = map(lambda x: x.to(device), patch_trg(batch.trg, opt.trg_pad_idx))
         
         # forward
+        optimizer.zero_grad()
+        pred= model(src_seq, trg_seq)
         
         # backward and update parameters
+        loss, n_correct, n_word = cal_performance( pred, gold, opt.trg_pad_idx, smoothing=smoothing)
+        loss.backward()
+        optimizer.step_and_update_lr()
         
         # note keeping
-        pass
-    pass
-
+        n_word_total += n_word
+        n_word_correct += n_correct
+        total_loss += loss.item()
+        
+    loss_per_word = total_loss/n_word_total
+    accuracy = n_word_correct/n_word_total
+    return loss_per_word, accuracy
+    
 def eval_epoch(model, validation_data, device, opt):
     """Epoch operation in evaluation phase"""
     pass
 
+
+# opt 는 어디서 오는거지?
+# training_data 어떤 객체지?
 def train(model, training_data, validation_data, optimizer, device, opt):
     """Start Training"""
-    pass
+    
+    # Use tensorboard to plot curves, e.g. perplexity, accuracy, learning rate
+    if opt.use_tb:
+        print("[Info] Use Tensorboard")
+        from torch.utils.tensorboard import SummaryWriter
+        tb_writer = SummaryWriter(log_dir=os.path.join(opt.output_dir, 'tensorboard'))
+        
+    log_train_file = os.path.join(opt.output_dir, 'train.log')
+    log_valid_file = os.path.join(opt.output_dir, 'valid.log')
+    
+    print('[Info] Training performance will be written to file: {} and {}'.format(
+        log_train_file, log_valid_file
+    ))
+    
+    with open(log_train_file, 'w') as log_tf, open(log_valid_file, 'w') as log_vf:
+        log_tf.write('epoch,loss,ppl,accuracy\n')
+        log_vf.write('epoch,loss,ppl,accuracy\n')
+    
+    # format 문법 보기
+    def print_performances(header, ppl, accu, start_time, lr):  # 'Training',  train_ppl, train_accu, start, lr
+        print('  - {header:12} ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, lr: {lr:8.5f}, elapse: {elapse:3.3f} min'
+             .format(header=f"({header})", 
+                            ppl=ppl,accu=100*accu, 
+                            elapse=(time.time()-start_time)/60, 
+                            lr=lr))
+    
+    #valid_accus = []
+    valid_losses = []
+    for epoch_i in range(opt.epoch):
+        print('[ Epoch', epoch_i, ']')
+        
+        start = time.time()
+        train_loss, train_accu = train_epoch(
+            model, training_data, optimizer, opt, device, smoothing=opt.label_smoothing)
+        
+        
+        
+        
+        pass
+    
+    
+    
 
 def main():
     
@@ -113,5 +170,5 @@ def prepare_dataloaders(opt, device):
     pass
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
