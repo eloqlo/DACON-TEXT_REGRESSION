577ecae9a210ec71e75e88892dea5a2294895a9b
Lee Jaehyeong <76482379+goldpink@users.noreply.github.com>
2022-08-09 00:04:58 +0900

    train_clone.py 이해 완료

--- train_clone.py
+++ train_clone.py
@@ -1,6 +1,7 @@
 import argparse
 import math
 import time
+from zlib import DEF_MEM_LEVEL
 import dill as pickle
 from tqdm import tqdm
 import numpy as np
@@ -11,10 +12,11 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from torchtext.data import Field, Dataset, BucketIterator
-from torchtext.datasets import TextclassificationDataset
+from torchtext.datasets import TextclassificationDataset, TranslationDataset
 
 import model.Constants as Constants
-from model.Models import BERT_like
+from model.Models import BERT_like, Transformer
+from model.Optim import ScheduledOptim
 # from model.Optim import ScheduledOptim
 
 # not yet
@@ -73,7 +75,7 @@ def train_epoch(model, training_data, optimizer, opt, device, smoothing):
     total_loss, n_word_total, n_word_correct = 0,0,0
     
     desc = '  - (Training)   '
-    for batch in tqdm(training_data, mininteraval=2, desc=desc, leave=False):
+    for batch in tqdm(training_data, mininteraval=2, desc=desc, leave=False):   # ? dataloader을 iteration 시키면 그 나오는 값들 형식이 궁금하다. 어떻게 이용하라고 줄까?
         
         # prepare data
         src_seq = patch_src(batch.src, opt.src_pad_idx).to(device)
@@ -96,7 +98,9 @@ def train_epoch(model, training_data, optimizer, opt, device, smoothing):
     loss_per_word = total_loss/n_word_total
     accuracy = n_word_correct/n_word_total
     return loss_per_word, accuracy
-    
+
+
+# TODO
 def eval_epoch(model, validation_data, device, opt):
     """Epoch operation in evaluation phase"""
     pass
@@ -106,6 +110,8 @@ def eval_epoch(model, validation_data, device, opt):
 # training_data 어떤 객체지?
 def train(model, training_data, validation_data, optimizer, device, opt):
     """Start Training"""
+    # ? opt 는 뭐지
+    
     
     # Use tensorboard to plot curves, e.g. perplexity, accuracy, learning rate
     if opt.use_tb:
@@ -132,18 +138,53 @@ def train(model, training_data, validation_data, optimizer, device, opt):
                             elapse=(time.time()-start_time)/60, 
                             lr=lr))
     
-    #valid_accus = []
+    valid_accus = []
     valid_losses = []
     for epoch_i in range(opt.epoch):
         print('[ Epoch', epoch_i, ']')
         
+        # TRAIN
         start = time.time()
         train_loss, train_accu = train_epoch(
             model, training_data, optimizer, opt, device, smoothing=opt.label_smoothing)
+        train_ppl = math.exp(min(train_loss, 100))  # ? 저 ppl은 뭐지?
+        # Current lr
+        lr = optimizer._optimizer.param_groups[0]['lr'] # ? optimizer 내부에 _optimizer/param_groups[0] 에는 뭐가있는거야?
+        print_performances('Training', train_ppl, train_accu, start, lr)
         
+        # VALIDATION
+        start = time.time()
+        valid_loss, valid_accu = eval_epoch(model, validation_data, device, opt)      # TODO
+        valid_ppl = math.exp(min(valid_loss, 100))
+        print_performances('Validation', valid_ppl, valid_accu, start, start, lr)
         
-        pass
-    
+        valid_losses += [valid_loss]    # * 제일 valid loss 작은 모델 선별위한 비교 set
+        
+        checkpoint = {'eopch': epoch_i, 'settings': opt, 'model': model.state_dict()}   # ? model.state_dict() 는 무슨 결과를 주지?
+        
+        # save_mode 옵션따라 모델 저장 처리.
+        if opt.save_mode == 'all':
+            model_name = 'model_accu_{accu:3.3f}.chkpt'.format(accu=100*valid_accu)
+            torch.save(checkpoint, model_name)  # ? 동작
+        elif opt.save_mode == 'best':
+            model_name = 'model.chkpt'
+            if valid_loss <= min(valid_losses): # TODO <= 가 아니라 == 도 되겠다.
+                torch.save(checkpoint, os.path.join(opt.output_dir, model_name))
+                print('   - [Info] The checkpoint file has been updated to lower valid_loss model !')
+        
+        with open(log_train_file, 'a') as log_tf, open(log_valid_file, 'a') as log_vf:  # * 'a' : 존재하는 파일에 추가하는 모드
+            log_tf.write('{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n'.format(
+                epoch=epoch_i, loss=train_loss, ppl=train_ppl, accu=100*train_accu
+            ))            
+            log_vf.write('{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n'.format(
+                epoch=epoch_i, loss=valid_loss, ppl=valid_ppl, accu=100*valid_accu
+            ))            
+        
+        # TODO 텐서보드 API 익히기.
+        if opt.use_tb:
+            tb_writer.add_scalars('ppl', {'train': train_ppl, 'val': valid_ppl}, epoch_i)
+            tb_writer.add_scalars('accuracy', {'train': train_accu*100, 'val':valid_accu*100}, epoch_i)
+            tb_writer.add_scalars('learning_rate', lr, epoch_i)
     
     
 
@@ -164,24 +205,148 @@ def main():
     parser.add_argument('-epoch', type=int, default=10)
     parser.add_argument('-b', '--batch_size', type=int, default=2048)
     
-    pass
+    parser.add_argument('-d_model', type=int, default=512)
+    parser.add_argument('-d_inner_hid', type=int, default=2048)
+    parser.add_argument('-d_k', type=int, default=64)
+    parser.add_argument('-d_v', type=int, default=64)
     
-    # some settings here
+    parser.add_argument('-n_head', type=int, default=8)
+    parser.add_argument('-n_layers', type=int, default=6)
+    parser.add_argument('-warmup', '--n_warmup_steps',type=int, default=4000)
+    parser.add_argument('-lr_mul', type=float, default=2.0)
+    parser.add_argument('-seed', type=int, default=None)
     
-    # some data laodings
+    parser.add_argument('-output_dir', type=str, default=None)
+    parser.add_argument('-use_tb', action='store_true') # * True 를 저장한다
+    parser.add_argument('-save_mode', type=str, choices=['all', 'best'], default='best')    # * choices 컨테이너에 포함되는지 type 변환이 수행한 후 검사
     
-    # some model generation
+    parser.add_argument('-cuda', action='store_true')
+    parser.add_argument('-label_smoothing', action='store_true')
     
-    # some optimizer generation
-    
-    pass
+    opt = parser.parse_args()
+    opt.d_word_vec = opt.d_model
 
+    if opt.seed is not None:
+        torch.manual_seed(opt.seed)
+        torch.backends.cudnn.benchmark = False # ?
+        np.random.seed(opt.seed)
+        random.seed(opt.seed)
+    
+    if not opt.output_dir:
+        print('No experiment result will be saved.')
+        raise
+    
+    if not os.path.exists(opt.output_dir):
+        os.makedirs(opt.output_dir)
+    
+    if opt.batch_size < 2048 and opt.n_warmup_steps <= 4000:
+        print('[Warning] The warmup steps may be not enough. \n'\
+            '(sz_b, warmup) = (2048,4000) is the official setting.\n'\
+            'Using smaller batch with out longer wramup may cause '\
+            'the warmup stage ends with only little data trained.'
+            )
+    
+    device = torch.device('cuda' if opt.cuda else 'cpu')
+    
+    #=============== LOADING DATASETS ================#
+    
+    if all((opt.train_path, opt.val_path)):
+        training_data, validation_data = prepare_dataloaders_from_bpe_files(opt, device)
+    elif opt.data_pkl:
+        training_data, validation_data = prepare_dataloaders(opt, device)
+    else:
+        raise
+    
+    print(opt)
+        
+
+    transformer = Transformer(
+        opt.src_vocab_size,
+        opt.trg_vocab_size,
+        src_pad_idx = opt.src_pad_idx,
+        trg_pad_idx = opt.trg_pad_idx,
+        trg_emb_prj_weight_sharing=opt.proj_share_weight,
+        emb_src_trg_weight_sharing=opt.embs_share_weight,
+        d_k=opt.d_k,
+        d_v=opt.d_v,
+        d_model=opt.d_model,
+        d_word_vec=opt.d_word_vec,
+        d_inner=opt.d_inner_hid,
+        n_layers=opt.n_layers,
+        n_head=opt.n_head,
+        dropout=opt.dropout,
+        scale_emb_or_prj=opt.scale_emb_or_prj
+    ).to(device)
+    
+    optimizer = ScheduledOptim(
+        optim.Adam(transformer.parameters(), betas=(0.9, 0.98), eps=1e-09,),
+        opt.lr_mul, opt.d_model, opt.n_warmup_steps)
+    
+    train(transformer, training_data, validation_data, optimizer, device, opt)
+    
+    
+# TODO bpe 복습
 def prepare_dataloaders_from_bpe_files(opt, device):
-    pass
+    batch_size = opt.batch_size
+    MIN_FREQ = 2
+    if not opt.embs_share_weight:
+        raise
+
+    data = pickle.load(open(opt.data_pkl, 'rb'))
+    MAX_LEN = data['settings'].max_len
+    field = data['vocab']
+    fields = (field, field)
+    
+    def filter_examples_with_length(x):
+        return len(vars(x)['src']) <= MAX_LEN and len(vars(x)['trg']) <= MAX_LEN    # * vars(object) -> __dict__, 멤버들 사전형으로 반환 : 여기선 src, trg 길이를 의미.
+    
+    train = TranslationDataset(
+        fields=fields,
+        path=opt.train_path,
+        exts=('.src', '.trg'),
+        filter_pred=filter_examples_with_length
+    )
+    val = TranslationDataset(
+        fields=fields,
+        path=opt.val_path,
+        exts=('.src', '.trg'),
+        filter_pred=filter_examples_with_length
+    )
+    
+    opt.max_token_seq_len = MAX_LEN + 2
+    opt.src_pad_idx = opt.trg_pad_idx = field.vocab.stoi[Constants.PAD_WORD]
+    opt.src_vocab_size = opt.trg_vocab_size = len(field.vocab)
+    
+    train_iterator = BucketIterator(train, batch_size=batch_size, device=device, train=True)
+    val_iterator = BucketIterator(val, batch_size=batch_size, device=device)
+    return train_iterator, val_iterator
+        
 
 def prepare_dataloaders(opt, device):
-    pass
-
+    batch_size = opt.batch_size
+    data = pickle.load(open(opt.data_pkl, 'rb'))    # ? vocab 데이터가 어떻게 구성되어있는거지?
+    
+    opt.max_token_seq_len = data['settings'].max_len
+    opt.src_pad_idx = data['vocab']['src'].vocab.stoi[Constants.PAD_WORD]
+    opt.trg_pad_idx = data['vocab']['trg'].vocab.stoi[Constants.PAD_WORD]
+    
+    opt.src_vocab_size = len(data['vocab']['src'].vocab)
+    opt.trg_vocab_size = len(data['vocab']['trg'].vocab)
+    
+    #============ PREPARING MODEL =============#
+    if opt.embs_share_weight:
+        assert data['vocab']['src'].vocab.stoi == data['vocab']['trg'].vocab.stoi, \
+            'To sharing word embedding the src/trg word2idx table shall be the same.'
+    
+    fields = {'src': data['vocab']['src'], 'trg':data['vocab']['trg']}
+    
+    train = Dataset(examples=data['train'], fields=fields)
+    val = Dataset(examples=data['valid'], fields=fields)
+    
+    train_iterator = BucketIterator(train, batch_size=batch_size, device=device, train=True)
+    val_iterator = BucketIterator(val, batch_size=batch_size, device=device)
+    
+    return train_iterator, val_iterator
 
 if __name__ == '__main__':
     main()
